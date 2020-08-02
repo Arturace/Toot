1f0f4af82d073ef749ec05b56dc54187a06b6e85
Artur Madjidov <artur.madjidov98@gmail.com>
2020-08-02 14:56:05 -0400

    Add TootEmphasizer doc

--- src/TootEmphasizer.d.ts
+++ src/TootEmphasizer.d.ts
@@ -1,5 +1,11 @@
 import { TootStep } from "./TootStep";
+
+/**
+ * Responsable for emphasizing and deemphasizing what is sneeded for a given TootStep
+ */
 export interface ITootEmphasizer {
+  /** Emphasize a TootStep */
   emphasize: (step: TootStep) => Promise<void>;
+  /** Deemphasize a TootStep */
   deemphasize: (step: TootStep) => Promise<void>;
 }
