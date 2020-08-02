61fdd8429be0d931e06209934a2ec6e1fb823ac4
Artur Madjidov <artur.madjidov98@gmail.com>
2020-08-02 14:55:56 -0400

    Add TootDisplay doc

--- src/TootDisplay.d.ts
+++ src/TootDisplay.d.ts
@@ -1,15 +1,24 @@
 import { TootStep } from "./TootStep";
 
+/** Responsable for displaying TootStep information & interactivity */
 export interface ITootDisplay {
+  /** Sets the title of a TootStep */
   setTitle(title: string): void;
+  /** Passes the description of a TootStep */
   setDescription(desc: string): void;
+  /** Set the callback that will continue the turoial */
   setNextCallback(callback: () => void);
+  /** Set the callback that will go back 1 step of the turoial */
   setPreviousCallback(callback: () => void);
+  /** Set the callback that will go stop the turoial */
   setStopCallback(callback: () => void);
+  /** WIll show the data & interactive elements of a TootStep */
   show: (stepIndex: number, tootLength: number) => Promise<void>;
+  /** WIll hide the data & interactive elements of a TootStep */
   hide: (stepIndex: number, tootLength: number) => Promise<void>;
 }
 
+/** Generates a ITootDisplay instance, for a given TootStep */
 export interface ITootDisplayGenerator {
   (step: TootStep): ITootDisplay;
 }
